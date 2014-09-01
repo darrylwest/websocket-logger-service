e22cb3711175b0c5a1b693dffff910cddfc34e83
darryl west <darryl.west@RainCitySoftware.com>
2014-09-01 10:35:48 -0700

    added open session request to service and client

--- examples/client.js
+++ examples/client.js
@@ -5,11 +5,31 @@ var dash = require('lodash'),
     LoggerService = require( __dirname + '/../index' ),
     MessageHub = require( 'node-messaging-commons' ),
     hub = MessageHub.createInstance( config ),
-    consumer = hub.createConsumer( LoggerService.DEFAULT_CHANNEL );
+    cid = 'logclient-' + Math.random().toString(20),
+    consumer = hub.createConsumer( LoggerService.DEFAULT_CHANNEL),
+    producer = hub.createProducer( '/' + cid );
 
 consumer.onMessage(function(msg) {
     if (msg.ssid === config.appkey) {
         console.log( msg );
+
+        // request to change log levels, shutdown, re-open session, etc...
     }
 });
 
+producer.onMessage(function(msg) {
+    console.log('<< ', msg);
+});
+
+var requestSession = function() {
+    var request = {
+        cid:cid,
+        action:'openSession'
+    };
+
+    console.log( '>> ', request );
+
+    consumer.publish( request );
+};
+
+requestSession();
